ec29acebca36f922054976d3f9ce13a5f7ed4887
Eddy Martin <eddypmartin@gmail.com>
2025-02-04 16:39:01 -0500

    s3c2 ajout d'un filtre par catégorie dans functions.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="global">
+        <section class="footer__contact">
+            <h2 class="footer__titre">Contact</h2>
+            <p class="footer__courriel">
+        </section>
+        <section class="footer__horaire">
+            <h2 class="footer__titre">Horaire</h2>
+            <p class="footer__jour">Lundi au vendredi</p>
+            <p class="footer__heure">8h00 à 17h00</p>
+        </section>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cLUB DE VOYAGE</title>
+    <!-- link rel="stylesheet" href="normalize.css" -->
+    <!-- link rel="stylesheet" href="style.css" -->
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <header>
+        <div class="entete">
+            <figure class="entete__logo">
+                <img
+                    src="<?php echo get_template_directory_uri() . '/images/logo_tuto.png' ?>"
+                    alt="logo">
+            </figure>
+            <label for="chk-burger" class="entete__burger">
+                <img
+
+                    src="https://s2.svgbox.net/hero-solid.svg?ic=menu-alt-1&color=000" width="32" height="32">
+            </label>
+            <input type="checkbox" name="" id="chk-burger" class="chk-burger">
+            <div class="entete__navigation">
+                <nav class="entete__menu">
+                    <ul class="menu">
+                        <li class="menu__item"><a href="" class="menu__item__a">Aventure</a></li>
+                        <li class="menu__item"><a href="" class="menu__item__a">Zen</a></li>
+                        <li class="menu__item"><a href="" class="menu__item__a">Culturel</a></li>
+                        <li class="menu__item"><a href="" class="menu__item__a">Sport</a></li>
+                        <li class="menu__item"><a href="" class="menu__item__a">Accueil</a></li>
+                        <li class="menu__item"><a href="" class="menu__item__a">Aventure</a></li>
+                        <li class="menu__item"><a href="" class="menu__item__a">Zen</a></li>
+                        <li class="menu__item"><a href="" class="menu__item__a">Culturel</a></li>
+                        <li class="menu__item"><a href="" class="menu__item__a">Sport</a></li>
+                    </ul>
+                </nav>
+                <form class="recherche">
+                    <input type="text" placeholder="Rechercher" class="recherche__input">
+                    <img class="recherche__img" src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000" width="16" height="16">
+                </form>
+            </div>
+        </div>
+    </header>
