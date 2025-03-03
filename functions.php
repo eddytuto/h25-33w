18f69676378d8e6d0a0ed58bc83c9df38605175e
Eddy Martin <eddypmartin@gmail.com>
2025-03-02 22:24:30 -0500

    s6c1 option custom-logo

--- functions.php
+++ functions.php
@@ -5,6 +5,8 @@ function mon_theme_supports()
     add_theme_support('title-tag');
     add_theme_support('menus');
     add_theme_support('post-thumbnails');
+    add_theme_support('custom-logo');
+    add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption'));
 }
 add_action('after_setup_theme', 'mon_theme_supports');
 
